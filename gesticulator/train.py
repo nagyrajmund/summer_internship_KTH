5b74f92c67ea36cf6da5f714cc18ca8020005845
nagyrajmund <rajmund977@gmail.com>
2020-07-11 07:05:05 +0200

    Save tensorboard logs to the results folder instead of lightning_logs

--- gesticulator/train.py
+++ gesticulator/train.py
@@ -7,7 +7,8 @@ import torch
 from config.model_config import construct_model_config_parser
 from gesticulator.model import My_Model
 from pytorch_lightning import Trainer
-from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
+from pytorch_lightning.logging import TensorBoardLogger
+
 
 from visualization.motion_visualizer.generate_videos import generate_videos
 SEED = 2334
@@ -16,48 +17,24 @@ np.random.seed(SEED)
 
 def main(hparams):
     model = My_Model(hparams)
+    logger = create_logger(model.save_dir)
 
-    # DEFAULTS used by Trainer
-    early_stop_callback = EarlyStopping(
-        monitor='avg_val_loss',
-        min_delta=0.00,
-        patience=5,
-        verbose=False,
-        mode='min'
-    )
-    
-    trainer = Trainer.from_argparse_args(hparams)
+    trainer = Trainer.from_argparse_args(hparams, logger=logger)
 
     if not hparams.no_train:
         trainer.fit(model)
     
-    # Save the model
-    save_path  = os.path.join(model.save_dir, 'trained_model_data')
-    print(f"Saving the model to {save_path}...")
-    model_data = {'state_dict' : model.state_dict(), 'hparams': model.hyper_params}
-    torch.save(model_data, save_path)
-
-    if hparams.no_test:
-        if hparams.save_videos_after_testing:
-            print("Please enable the testing procedure for saving the videos by removing the --no_test flag!")
-    else:
+    if not hparams.no_test:
         trainer.test(model)
-
-        if hparams.save_videos_after_testing:
-            save_videos(model.save_dir, hparams.run_name)
-
-
-def save_videos(save_dir, run_name):
-    """Generate the gesticulation videos for a test sequence."""
-    raw_gesture_path = os.path.join(save_dir, 'test_videos/raw_data')
-    output_dir = os.path.join(save_dir, 'test_videos')
-    data_pipe = 'utils/data_pipe.sav'
-
-    generate_videos(raw_input_folder=raw_gesture_path,
-                    output_folder=output_dir, 
-                    run_name=run_name,
-                    data_pipe_dir=data_pipe)
-
+   
+def create_logger(model_save_dir):
+    # str.rpartition(separator) cuts up the string into a 3-tuple of (a,b,c), where
+    #   a: everything before the last occurrence of the separator
+    #   b: the separator
+    #   c: everything after the last occurrence of the separator)
+    result_dir, _, run_name = model_save_dir.rpartition('/')
+    
+    return TensorBoardLogger(save_dir=result_dir, version=run_name, name="")
 
 def add_training_script_arguments(parser):
     parser.add_argument('--no_train', '-no_train', action="store_true",
@@ -70,7 +47,7 @@ def add_training_script_arguments(parser):
                         help="If set, generate test videos from the raw gesture data after"
                              "the testing phase is over.")
     return parser
-    
+
 if __name__ == '__main__':
     # Model parameters are added here
     parser = construct_model_config_parser()
