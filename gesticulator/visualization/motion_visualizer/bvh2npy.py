afb684281faac6fa40ad8cd8204c9e4d4002451c
Taras Kucherenko <tarask@kth.se>
2020-08-14 11:36:40 +0200

    Remove redundant coords which we don't model from visualization (fingers and legs)

--- gesticulator/visualization/motion_visualizer/bvh2npy.py
+++ gesticulator/visualization/motion_visualizer/bvh2npy.py
@@ -5,12 +5,17 @@ import argparse
 
 def convert_bvh2npy(bvh_file, npy_file):
     # read the coordinates
-    coords = read_bvh_to_array(bvh_file)
+    all_coords = read_bvh_to_array(bvh_file)
 
-    # shorten the sequence
-    output_vectors = coords[:6000]
+    # remove fingers and lower body
+    torso = all_coords[:,0:8]
+    right_arm = all_coords[:,9:12]
+    left_arm = all_coords[:,28:31]
 
-    np.save(npy_file, output_vectors)
+    modeled_coords = np.concatenate((torso, right_arm, left_arm), axis=1)
+    print(modeled_coords.shape)
+
+    np.save(npy_file, modeled_coords)
 
 
 if __name__ == "__main__":
@@ -24,7 +29,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--bvh_file",
         "-bvh",
-        default="/home/taras/Documents/Datasets/SpeechToMotion/Irish/raw/Cleaned&Fixed/BVH_60fps/_1/NaturalTalking_001.bvh",
+        default="/home/tarask/Documents/Experimental_Results/GENEA/GT_results/TestSeq001.bvh",
         help="Address to the bvh file",
     )
     parser.add_argument(
